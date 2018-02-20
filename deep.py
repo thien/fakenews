79b59379ab77b91c033dd910d0a791f1e12baf59
Thien <thien@users.noreply.github.com>
2018-02-20 23:56:19 +0000

    initial commit of placeholder functions

---
+++ deep.py
@@ -0,0 +1,24 @@
+# I hope you like some deep learning
+# import spacy
+
+
+# -----------------------------
+# Pre Processing and Feature Extraction
+# -----------------------------
+
+# (5 marks)
+def word2vec():
+  return 0
+
+# -----------------------------
+# Classification
+# -----------------------------
+
+# Long Short-Term Memory (15 marks)
+def lstm():
+  return 0
+
+# Recursive Neural Network (10 marks)
+def rnn():
+  return 0
+
---
+++ shallow.py
@@ -0,0 +1,33 @@
+import textacy
+
+# -----------------------------
+# Pre Processing and Feature Extraction
+# -----------------------------
+
+def tf():
+  return 0
+
+def tfIdf():
+  return 0
+
+def ngram():
+  return 0
+
+# -----------------------------
+# Classification
+# -----------------------------
+
+def naive_bayes():
+  return 0
+
+def calculateAccuracy():
+  return 0
+
+def calculatePrecision():
+  return 0
+
+def calculateRecall():
+  return 0
+
+def calculateF1Measure():
+  return 0
